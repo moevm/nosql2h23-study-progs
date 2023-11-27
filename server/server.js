6537dc9815fe445509a740e42c79bb184ac00d9a
AlexandrovichValeria <71718459+AlexandrovichValeria@users.noreply.github.com>
2023-11-27 18:55:07 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,11 +5,31 @@ import cors from 'cors';
 import { parseFromString } from 'dom-parser';
 
 const app = express()
-const db = neo4j.driver("neo4j+s://e9b78945.databases.neo4j.io", neo4j.auth.basic('neo4j', 'vlRz2v2ZH2EMs6wFvnkONVPqJohorsec0qHmfVbFDy8'))
+const db = neo4j.driver("neo4j+s://c6436e2d.databases.neo4j.io", neo4j.auth.basic('neo4j', 'rFEUHES8XeDv6A-fDZtXIFO4hbMdwauqbGkpfLq8UJg'))
 
 app.use(cors())
 
 app.listen(4200)
+
+/// проверка
+const session = db.session();
+
+session
+    .run('MATCH (n) RETURN n LIMIT 5')
+    .then((result) => {
+      result.records.forEach((record) => {
+        console.log(record.get('n').properties);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      session.close();
+      db.close();
+});
+
+///
 
 app.get('/', (req, res) => {
     (async () => {
